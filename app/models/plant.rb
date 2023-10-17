be06df606cf7a9ede93f661a990a291e86eaf775
Harsh Patel <p.harsh2525@gmail.com>
2023-10-17 07:04:21 -0500

    added validation for plants

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :category
+
+  validates :name, :price, presence: true
+  validates :stock, numericality: { only_integers: true }
+  validates :price, numericality: true
 end
