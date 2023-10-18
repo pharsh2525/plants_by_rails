274473c6ee34ef545189b69058720cf6b4a4ab22
Harsh Patel <p.harsh2525@gmail.com>
2023-10-17 21:46:07 -0500

    trying to add data from crv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+Category.delete_all
+
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='categories';")
+
+filename = Rails.root.join('db/plant_store.csv')
+
+puts "Loading Plants from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+
+plants = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+plants.each do |x|
+  category_name = x['category']
+  plant_name = x['name']
+  description = x['description']
+  price =  x['price'].gsub(/[$,]/, '').to_f
+
+  category = Category.find_or_create_by(name: category_name)
+
+  # Print the category and plant details for debugging
+  puts "Category: #{category_name}"
+  puts "Plant: #{plant_name}, Description: #{description}, Price: #{price}"
+
+  plant = category.plants.create(
+    name: plant_name,
+    description:,
+    price:,
+    stock: rand(1..100)
+  )
+
+  # Print a message indicating whether the plant was created
+  if plant.persisted?
+    puts 'Plant created successfully.'
+  else
+    puts 'Failed to create plant.'
+  end
+end
